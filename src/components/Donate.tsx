b65e41d54f498bcc40e0a9d92daa2c717401ffa4
inathan44 <inathan44@yahoo.com>
2024-11-27 10:26:59 -0500

    add donation receipt

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-import DonationPopUp from './DonationPopUp';
 import Navbar from './Navbar';
-import { Button } from './ui/button';
 
 interface Donation {
   amount: number;
@@ -26,10 +23,10 @@ const donations: Donation[] = [
 export default function Donate() {
   document.title = "Donate to David's Birthday Gift Fund";
 
-  const [showDonation, setShowDonation] = useState(false);
-  const handleDonationClick = () => {
-    setShowDonation(!showDonation);
-  };
+  // const [showDonation, setShowDonation] = useState(false);
+  // const handleDonationClick = () => {
+  //   setShowDonation(!showDonation);
+  // };
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-black text-white p-4'>
@@ -37,29 +34,12 @@ export default function Donate() {
       <h1 className='text-3xl font-bold mb-4'>
         Donate to David's Birthday Fund
       </h1>
-      <p className='mb-6 text-lg text-center'>
-        Donations are completely optional. How it will work is you will Venmo me
-        (Ian) an amount under $10. For accounting/tracking purposes, donations
-        will not be anonymous so everyone is sure that all money is accounted
-        for. I will not be taking a cut; all money will go to David . Any amount
-        over $10 will be refunded back to you. If you would rather Venmo him
-        directly, his Venmo is "@Devans_", and his birthday is 11/27, so please
-        send on that date. I have to manually update the webpage to show who
-        donated, so it will take up to two days for you to see your donation
-        reflected here. I will post a screenshot once all the money has been
-        sent. My venmo is set to private so he does not see the donations
-        (another reason donations are not anonymous).
-      </p>
+      <img className='px-6' src='Screenshot 2024-11-27 at 10.23.10 AM.png' />
+      <h1 className='text-lg text-green-500'>
+        Funds were already sent, no longer accepting donations. Thanks so
+        everyone who contributed / wrote a message!
+      </h1>
 
-      {showDonation && <DonationPopUp />}
-      {!showDonation && (
-        <Button
-          onClick={handleDonationClick}
-          className='bg-blue-600 text-white py-2 px-4 rounded transition hover:bg-blue-500'
-        >
-          Contribute to bday fund via Venmo
-        </Button>
-      )}
       <div className='mt-6'>
         {donations.map((donation: Donation, index: number) => (
           <div key={index} className='flex items-center gap-4'>
